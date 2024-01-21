1266289ab6601284c9a9cfc6c46d0bd75b66d3ed
yousseftawfeek <yousseftawfeek1000@gmail.com>
2024-01-21 13:39:52 +0000

    construction of the project

---
+++ README.md
@@ -0,0 +1,1 @@
+this is soring algorithms project by youssef & amira
---
+++ sort.h
@@ -0,0 +1,40 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - is doubly linked list node
+ *
+ * @n: int stored in the node
+ * @prev: pointer to previous element of list
+ * @next: Pointer to next element of list
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* printing functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* sorting algoritms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif /* SORT_H */
